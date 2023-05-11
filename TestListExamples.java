5f2caf13a351f46030a07335cc30c92bdd693989
EmmaWang0126 <130112932+EmmaWang0126@users.noreply.github.com>
2023-05-11 16:15:14 -0700

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,34 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "b");
+    List<String> right = Arrays.asList("c", "d", "e");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e");
+    assertEquals(expected, merged);
+  }
+  @Test(timeout = 500)
+  public void testMergeEmptyLists() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, merged);
+}
+  @Test 
+	public void testIndex() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/");
+    assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
+	}
+	@Test 
+	public void testSearch() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/search?q=Resonance");
+    String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
+
 }
